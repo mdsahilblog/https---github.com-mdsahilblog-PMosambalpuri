7cb9ecb55427c043f134f8d9ec851267ea473e7f
mdsahilblog <71087857+mdsahilblog@users.noreply.github.com>
2023-02-11 07:12:01 +0000

    changed today

--- app/Http/Livewire/Admin/Brand/Index.php
+++ app/Http/Livewire/Admin/Brand/Index.php
@@ -11,7 +11,7 @@ class Index extends Component
     public function rules()
     {
         return [
-           //nothing you can edit here
+           //nothing you can edit here today
         ];
     }
 
